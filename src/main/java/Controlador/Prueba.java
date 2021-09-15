07c07aae71b2888a8485215aefc3f25fab05b4b4
vivisfore15 <vivis@VIVIANA_FORERO>
2021-09-15 11:03:23 -0500

    Nuevo Metodo2

--- src/main/java/Controlador/Prueba.java
+++ src/main/java/Controlador/Prueba.java
@@ -7,4 +7,8 @@ public class Prueba {
 	public void metodo() {
 	JOptionPane.showMessageDialog(null,"Subiendo a Git");
 	}
+	
+	public void metodo2() {
+	JOptionPane.showMessageDialog(null,"Commit 3");
+	}
 }
